5c21d75cd2bd72a2bc5b67073f2f2160202d0bb4
Stephen Souness <sounie@hotmail.com>
2015-07-20 14:51:19 +0100

    Added an experiment around indexOf for sorted list.

--- src/ArrayListApi.java
+++ src/ArrayListApi.java
@@ -1,11 +1,55 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Random;
 
 /**
  * Trying out some things in ArrayList
  */
 public class ArrayListApi {
     public static void main(String args[]) {
-       trimToSize();
+        indexOfSortedList();
+//       trimToSize();
+    }
+
+    private static void indexOfSortedList() {
+        // indexOf is a bit naive, so let's see if a sorted list can be made to behave better
+        ArrayList<Integer> sorted = new ArrayList<>();
+
+        Random random = new Random();
+
+        for (int i = 0; i < 50_000_000; i++) {
+            sorted.add(i);
+
+            // Introducing duplicates at random points
+            if (random.nextBoolean()) {
+                sorted.add(i);
+            }
+        }
+
+        final Integer target = Integer.valueOf(48_000_000);
+
+        long timeBefore = System.currentTimeMillis();
+        int anIndex = Collections.binarySearch(sorted, target,
+                (o1, o2) -> o1.compareTo(o2)
+        );
+
+        // We treat anIndex as starting position and move backwards until we establish the first instance
+        while (anIndex > 0 && sorted.get(anIndex - 1).equals(target)) {
+            anIndex--;
+        }
+        System.out.println("Duration binary approach " + (System.currentTimeMillis() - timeBefore));
+
+        long timeBeforeIndexOf = System.currentTimeMillis();
+        int indexOfVal = sorted.indexOf(target);
+
+        System.out.println("Duration indexOf " + (System.currentTimeMillis() - timeBeforeIndexOf));
+
+        System.out.println("IndexOf value " + indexOfVal);
+        System.out.println("IndexOf verification: " + sorted.get(indexOfVal));
+
+        System.out.println("Found at index " + anIndex);
+        System.out.println("DEBUG - item was - " + sorted.get(anIndex));
     }
 
     private static void trimToSize() {
